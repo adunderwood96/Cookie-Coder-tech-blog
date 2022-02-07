561ab8639dcdf76410aed8f8c208d364076b3953
Ashley <adunderwood96@gmail.com>
2022-02-07 14:18:39 -0500

    minor bug issues

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const homeRoutes = require('./homeRoutes');
-const dashboardRoutes = require('./dashboardRoutes');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use('/dashboard', dashboardRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
--- models/index.js
+++ models/index.js
@@ -1,4 +1,37 @@
 const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
+User.hasMany(Post, {
+    foreignKey: 'userID'
+});
 
-module.exports = { User };
+Post.belongsTo(User, {
+    foreignKey: 'userID'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'userID',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'postID',
+    onDelete: 'cascade',
+    hooks: true
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'userID',
+    onDelete: 'cascade',
+    hooks:true
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'postID',
+    onDelete: 'cascade',
+    hooks:true
+})
+
+module.exports = { User, Post, Comment };
